dcd428b17fe1df36c9f748d5204049cfa9ef2cf1
test-cursor <kierancagney@hotmail.com>
2026-02-14 16:18:51 +0100

    Initial Commit

---
+++ test-cursor/test_cursorApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_cursorApp.swift
+//  test-cursor
+//
+//  Created by Kieran Cagney on 14/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct test_cursorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
